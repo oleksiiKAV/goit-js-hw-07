2e525857d5ab1052604327d8f37d540c45815323
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-04-21 10:58:56 +0300

    caption added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -22,51 +22,61 @@ h1.style.display = "none";
 </li> */}
 const listItems = [];
 galleryItems.forEach(item => {
-    // <li> with class "gallery__item"
-    const li = document.createElement('li');
-    li.classList.add('gallery__item');
+   // <li> with class "gallery__item"
+   const li = document.createElement('li');
+   li.classList.add('gallery__item');
 
-    // add link <a> with class "gallery__link" and attr href
-    const link = document.createElement('a');
-    link.classList.add('gallery__link');
-    link.href = item.original;
+   // add link <a> with class "gallery__link" and attr href
+   const link = document.createElement('a');
+   link.classList.add('gallery__link');
+   link.href = item.original;
 
-    // Create <img> with class "gallery__image" and attr src и alt
-    const img = document.createElement('img');
-    img.classList.add('gallery__image');
-    img.src = item.preview;
-    img.alt = item.description;
+   // Create <img> with class "gallery__image" and attr src и alt
+   const img = document.createElement('img');
+   img.classList.add('gallery__image');
+   img.src = item.preview;
+   img.alt = item.description;
 
-    // Add action into img
-    // img.addEventListener('click', (event) => {
-    //     event.preventDefault();
+   // Add action into img
+   // img.addEventListener('click', (event) => {
+   //     event.preventDefault();
 
-    //     const fullImg = basicLightbox.create(`
-    //         <img src="${img.dataset.source}" alt="${img.alt}" />`);
-    //     fullImg.show();
-    //     // add Esc
-    //     document.addEventListener('keydown', (event) => {
-    //         if (event.key === 'Escape') {
-    //             fullImg.close();
-    //         }
-    //     });
-    // });
+   //     const fullImg = basicLightbox.create(`
+   //         <img src="${img.dataset.source}" alt="${img.alt}" />`);
+   //     fullImg.show();
+   //     // add Esc
+   //     document.addEventListener('keydown', (event) => {
+   //         if (event.key === 'Escape') {
+   //             fullImg.close();
+   //         }
+   //     });
+   // });
 
 
-    // Add <img> into <a>
-    link.appendChild(img);
+   // Add <img> into <a>
+   link.appendChild(img);
 
-    // Add <a> into <li>
-    li.appendChild(link);
+   // Add <a> into <li>
+   li.appendChild(link);
 
-    // Add <li> into <ul>
-    listItems.push(li);
-    // gallery.appendChild(li);
+   // Add <li> into <ul>
+   listItems.push(li);
+   // gallery.appendChild(li);
 
 });
 gallery.append(...listItems);
 // SimpleLightbox.caption=true
-let galleryBox = new SimpleLightbox('.gallery a');
+let galleryBox = new SimpleLightbox('.gallery a'
+   // , {
+   //    caption: true,
+   //    captionDelay: 250,
+   //    captionsData: "alt",
+   // }
+);
+galleryBox.options.caption = true;
+galleryBox.options.captionDelay = 250
+galleryBox.options.captionsData = "alt"
+// galleryBox.refresh()
 console.log(galleryBox)
 
     // gallery.next(); // Next Image
